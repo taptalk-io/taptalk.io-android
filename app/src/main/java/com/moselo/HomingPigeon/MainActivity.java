8348a49daa17b7c6081bb542a650c458c53ae017
Rionaldo <rionaldo.ling@gmail.com>
2018-08-10 14:41:13 +0700

    make activity navigate to activity in library

--- app/src/main/java/com/moselo/HomingPigeon/MainActivity.java
+++ app/src/main/java/com/moselo/HomingPigeon/MainActivity.java
@@ -1,14 +1,27 @@
 package com.moselo.HomingPigeon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ToasterMessage.showToast(this,"Ini Showing");
+        tvHelloWorld = findViewById(R.id.tv_hello);
+        tvHelloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LibraryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
