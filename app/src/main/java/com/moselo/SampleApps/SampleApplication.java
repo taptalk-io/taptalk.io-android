c6b44532aec74c8e2061536e7c29cb55d42e30e7
Rionaldo <rionaldo.ling@gmail.com>
2018-08-27 16:22:57 +0700

    add stetho

--- app/src/main/java/com/moselo/SampleApps/SampleApplication.java
+++ app/src/main/java/com/moselo/SampleApps/SampleApplication.java
@@ -2,6 +2,7 @@ package com.moselo.SampleApps;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
 import com.moselo.HomingPigeon.Helper.HomingPigeon;
 
 public class SampleApplication extends Application {
@@ -10,5 +11,10 @@ public class SampleApplication extends Application {
     public void onCreate() {
         super.onCreate();
         HomingPigeon.init(this);
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                        .build());
     }
 }
