dbb7976bfd28bd3521a8003850d3f2179e93adf0
Rionaldo <rionaldo.ling@gmail.com>
2018-08-29 20:26:28 +0700

    undeprecate room Model

--- homingpigeon/src/main/java/com/moselo/HomingPigeon/Model/RoomModel.java
+++ homingpigeon/src/main/java/com/moselo/HomingPigeon/Model/RoomModel.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@Deprecated
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
         "roomID",
