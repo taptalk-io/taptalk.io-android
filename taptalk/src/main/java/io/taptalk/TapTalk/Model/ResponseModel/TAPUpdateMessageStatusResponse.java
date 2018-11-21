1fedde088f6cfe705ea2b32849e71e4c18e1ec8c
Kevin <kevin@moselo.com>
2018-11-21 12:47:22 +0700

    - added message delivery status API model

---
+++ taptalk/src/main/java/io/taptalk/TapTalk/Model/ResponseModel/TAPUpdateMessageStatusResponse.java
@@ -0,0 +1,21 @@
+package io.taptalk.TapTalk.Model.ResponseModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class TAPUpdateMessageStatusResponse {
+    @JsonProperty("updatedMessageIDs") private List<String> updatedMessageIDs;
+
+    public TAPUpdateMessageStatusResponse(List<String> updatedMessageIDs) {
+        this.updatedMessageIDs = updatedMessageIDs;
+    }
+
+    public List<String> getUpdatedMessageIDs() {
+        return updatedMessageIDs;
+    }
+
+    public void setUpdatedMessageIDs(List<String> updatedMessageIDs) {
+        this.updatedMessageIDs = updatedMessageIDs;
+    }
+}
