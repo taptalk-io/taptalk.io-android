011a2c9dda383a9020400a3b6c9943c8e1866286
Rionaldo <rionaldo.ling@gmail.com>
2019-01-29 15:51:44 +0700

    delete on Login Success from TAP Listener

--- taptalk/src/main/java/io/taptalk/TapTalk/Interface/TapTalkInterface.java
+++ taptalk/src/main/java/io/taptalk/TapTalk/Interface/TapTalkInterface.java
@@ -9,7 +9,6 @@ import io.taptalk.TapTalk.Model.TAPUserModel;
 
 public interface TapTalkInterface {
     void onRefreshTokenExpiredOrInvalid();
-    void onLoginSuccess(TAPUserModel myUserModel);
     void onUserProfileClicked(Activity activity, TAPUserModel userModel);
     void onCustomKeyboardItemClicked(Activity activity, TAPCustomKeyboardItemModel customKeyboardItemModel, TAPUserModel activeUser, TAPUserModel otherUser);
     List<TAPCustomKeyboardItemModel> onRequestCustomKeyboardItems(TAPUserModel activeUser, TAPUserModel otherUser);
--- taptalk/src/main/java/io/taptalk/TapTalk/Listener/TAPListener.java
+++ taptalk/src/main/java/io/taptalk/TapTalk/Listener/TAPListener.java
@@ -10,7 +10,6 @@ import io.taptalk.TapTalk.Model.TAPUserModel;
 
 public abstract class TAPListener implements TapTalkInterface {
     @Override public void onRefreshTokenExpiredOrInvalid() {}
-    @Override public void onLoginSuccess(TAPUserModel myUserModel) {}
     @Override public void onUserProfileClicked(Activity activity, TAPUserModel userModel) {}
     @Override public void onCustomKeyboardItemClicked(Activity activity, TAPCustomKeyboardItemModel customKeyboardItemModel, TAPUserModel activeUser, TAPUserModel otherUser) {}
     @Override public List<TAPCustomKeyboardItemModel> onRequestCustomKeyboardItems(TAPUserModel activeUser, TAPUserModel otherUser) {
