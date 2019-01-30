f376bfabb6b84ecbc3e0228346fdf7ff0a49d7ce
Rionaldo <rionaldo.ling@gmail.com>
2019-01-30 11:51:28 +0700

    error fixing when auth ticket is "" or null

--- taptalk/src/main/java/io/taptalk/TapTalk/Interface/TAPLoginInterface.java
+++ taptalk/src/main/java/io/taptalk/TapTalk/Interface/TAPLoginInterface.java
@@ -1,6 +1,8 @@
 package io.taptalk.TapTalk.Interface;
 
+import io.taptalk.TapTalk.Model.TAPErrorModel;
+
 public interface TAPLoginInterface {
     void onLoginSuccess();
-    void onLoginFailed(String reason);
+    void onLoginFailed(TAPErrorModel error);
 }
