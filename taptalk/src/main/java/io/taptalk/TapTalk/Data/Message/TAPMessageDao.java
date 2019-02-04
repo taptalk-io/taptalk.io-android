29bb18d94a637b6aaba8ec1f4d1b6efdf1a02034
Rionaldo <rionaldo.ling@gmail.com>
2019-02-04 13:01:45 +0700

    fix image by change how to query

--- taptalk/src/main/java/io/taptalk/TapTalk/Data/Message/TAPMessageDao.java
+++ taptalk/src/main/java/io/taptalk/TapTalk/Data/Message/TAPMessageDao.java
@@ -33,50 +33,25 @@ public interface TAPMessageDao {
     @Query("select * from Message_Table order by created desc")
     LiveData<List<TAPMessageEntity>> getAllMessage();
 
-    @Query("select messageID, localID, roomID, roomName, roomColor, roomType, " +
-            " roomImage, type, body, created, data, quote, replyTo, forwardFrom, " +
-            "userID, xcUserID, userFullName, userImage, username, userEmail, " +
-            "userPhone, userRole, lastLogin, requireChangePassword, userCreated, userUpdated, " +
-            "recipientID, isRead, isDelivered, isHidden, isDeleted, isSending, isFailedSend" +
-            " from Message_Table where RoomID like :roomID order by created desc limit " + numOfItem)
+    @Query("select * from Message_Table where RoomID like :roomID order by created desc limit " + numOfItem)
     List<TAPMessageEntity> getAllMessageListDesc(String roomID);
 
-    @Query("select messageID, localID, roomID, roomName, roomColor, roomType," +
-            " roomImage, type, body, created, data, quote, replyTo, forwardFrom, " +
-            "userID, xcUserID, userFullName, userImage, username, userEmail, " +
-            "userPhone, userRole, lastLogin, requireChangePassword, userCreated, userUpdated, " +
-            "recipientID, isRead, isDelivered, isHidden, isDeleted, isSending, isFailedSend" +
-            " from Message_Table where RoomID like :roomID order by created desc limit " + numOfItem)
+    @Query("select * from Message_Table where RoomID like :roomID order by created desc limit " + numOfItem)
     List<TAPMessageEntity> getAllMessageListAsc(String roomID);
 
-    @Query("select messageID, localID, roomID, roomName, roomColor, roomType, " +
-            "roomImage, type, body, created, data, quote, replyTo, forwardFrom, " +
-            "userID, xcUserID, userFullName, userImage, username, userEmail, " +
-            "userPhone, userRole, lastLogin, requireChangePassword, userCreated, userUpdated, " +
-            "recipientID, isRead, isDelivered, isHidden, isDeleted, isSending, isFailedSend " +
-            "from Message_Table where " +
+    @Query("select * from Message_Table where " +
             "created in (select distinct created from Message_Table where created < :lastTimestamp and RoomID like :roomID order by created desc limit " + numOfItem + " ) " +
             "and RoomID like :roomID order by created desc")
     List<TAPMessageEntity> getAllMessageTimeStamp(Long lastTimestamp, String roomID);
 
-    @Query("select messageID, localID, roomID, roomName, roomColor, roomType," +
-            " roomImage, type, body, created, data, quote, replyTo, forwardFrom, " +
-            "userID, xcUserID, userFullName, userImage, username, userEmail, " +
-            "userPhone, userRole, lastLogin, requireChangePassword, userCreated, userUpdated, " +
-            "recipientID, isRead, isDelivered, isHidden, isDeleted, isSending, isFailedSend" +
-            " from Message_Table where body like :keyword order by created desc")
+    @Query("select * from Message_Table where body like :keyword order by created desc")
     List<TAPMessageEntity> searchAllMessages(String keyword);
 
-    @Query("select localID, userID, xcUserID, userFullName, userImage, username, userEmail, " +
-            "userPhone, userRole, lastLogin, requireChangePassword, userCreated, userUpdated, " +
-            "roomName, secondQuery.roomID, roomType, body, created, isDelivered, isFailedSend, " +
-            "isSending, recipientID, type from (select roomID, max(created) as max_created from Message_Table group by roomID) secondQuery join Message_Table firstQuery on firstQuery.roomID = secondQuery.roomID and firstQuery.created = secondQuery.max_created order by firstQuery.created desc")
+    @Query("select * from (select roomID, max(created) as max_created from Message_Table group by roomID) secondQuery join Message_Table firstQuery on firstQuery.roomID = secondQuery.roomID and firstQuery.created = secondQuery.max_created " +
+            "group by firstQuery.roomID order by firstQuery.created desc")
     List<TAPMessageEntity> getAllRoomList();
 
-    @Query("select localID, userID, xcUserID, userFullName, userImage, username, userEmail, " +
-            "userPhone, userRole, lastLogin, requireChangePassword, userCreated, userUpdated, " +
-            "roomName, secondQuery.roomID, roomType, body, created, isDelivered, isFailedSend, " +
-            "isSending, recipientID, type from (select roomID, max(created) as max_created from Message_Table group by roomID) " +
+    @Query("select * from (select roomID, max(created) as max_created from Message_Table group by roomID) " +
             "secondQuery join Message_Table firstQuery on firstQuery.roomID = secondQuery.roomID and firstQuery.created = secondQuery.max_created where roomName like :keyword order by firstQuery.created desc")
     List<TAPMessageEntity> searchAllChatRooms(String keyword);
 
