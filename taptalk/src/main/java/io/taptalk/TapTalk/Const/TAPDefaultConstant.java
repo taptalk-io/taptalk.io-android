5daea52c0dca2c1a849b2e8e02e51610e79b91e6
Rionaldo <rionaldo.ling@gmail.com>
2019-02-15 18:13:05 +0700

    Add Auto Start Permission Dialog to receive Notification when apps is killed (in China roms)

--- taptalk/src/main/java/io/taptalk/TapTalk/Const/TAPDefaultConstant.java
+++ taptalk/src/main/java/io/taptalk/TapTalk/Const/TAPDefaultConstant.java
@@ -1,5 +1,9 @@
 package io.taptalk.TapTalk.Const;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+
 import io.taptalk.TapTalk.Helper.TapTalk;
 
 public class TAPDefaultConstant {
@@ -195,6 +199,23 @@ public class TAPDefaultConstant {
         public static final String BASE_URL_SOCKET_DEVELOPMENT = "https://hp-dev.moselo.com:8080/";
         public static final String BASE_WSS_DEVELOPMENT = "wss://hp-dev.moselo.com:8080/pigeon";
     }
+
+    public static final Intent[] AUTO_START_INTENTS = {
+            new Intent().setComponent(new ComponentName("com.samsung.android.lool",
+                    "com.samsung.android.sm.ui.battery.BatteryActivity")),
+            new Intent("miui.intent.action.OP_AUTO_START").addCategory(Intent.CATEGORY_DEFAULT),
+            new Intent().setComponent(new ComponentName("com.miui.securitycenter", "com.miui.permcenter.autostart.AutoStartManagementActivity")),
+            new Intent().setComponent(new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity")),
+            new Intent().setComponent(new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity")),
+            new Intent().setComponent(new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.permission.startup.StartupAppListActivity")),
+            new Intent().setComponent(new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.startupapp.StartupAppListActivity")),
+            new Intent().setComponent(new ComponentName("com.oppo.safe", "com.oppo.safe.permission.startup.StartupAppListActivity")),
+            new Intent().setComponent(new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.AddWhiteListActivity")),
+            new Intent().setComponent(new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.BgStartUpManager")),
+            new Intent().setComponent(new ComponentName("com.vivo.permissionmanager", "com.vivo.permissionmanager.activity.BgStartUpManagerActivity")),
+            new Intent().setComponent(new ComponentName("com.asus.mobilemanager", "com.asus.mobilemanager.entry.FunctionActivity")).setData(
+                    Uri.parse("mobilemanager://function/entry/AutoStart"))
+    };
 
     public static final String K_REFRESH_TOKEN = "kTAPRefreshToken";
     public static final String K_REFRESH_TOKEN_EXPIRY = "kTAPRefreshTokenExpiry";
---
+++ taptalk/src/main/java/io/taptalk/TapTalk/Helper/TAPAutoStartPermission.java
@@ -0,0 +1,41 @@
+package io.taptalk.TapTalk.Helper;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
+import android.util.Log;
+
+import java.util.List;
+
+import io.taptalk.TapTalk.Const.TAPDefaultConstant;
+
+public class TAPAutoStartPermission {
+
+    private static TAPAutoStartPermission instance;
+
+    public static TAPAutoStartPermission getInstance() {
+        return null == instance ? instance = new TAPAutoStartPermission() : instance;
+    }
+
+    public void showPermissionRequest(Context context) {
+        for (Intent intent : TAPDefaultConstant.AUTO_START_INTENTS)
+            if (context.getPackageManager().resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY) != null) {
+                new TapTalkDialog.Builder(context)
+                        .setTitle("Enable AutoStart")
+                        .setMessage("Please allow auto start in settings to receive chat notifications from Moselo.")
+                        .setPrimaryButtonTitle("Allow")
+                        .setPrimaryButtonListener(true, v -> {
+                            try {
+                                context.startActivity(intent);
+                            } catch (Exception e) {
+                                Log.e("><><><", "showPermissionRequest: ",e );
+                                e.printStackTrace();
+                            }
+                        })
+                        .show();
+            }
+    }
+}
