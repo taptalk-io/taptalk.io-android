cac41e9f6281dc3631cce1c4b3de4dced68d55f0
Rionaldo <rionaldo.ling@gmail.com>
2019-02-22 22:43:02 +0700

    fix unresolved dependencies add custom tab view chrome make TAP Web Browser Activity

---
+++ taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TAPWebBrowserActivity.kt
@@ -0,0 +1,13 @@
+package io.taptalk.TapTalk.View.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.taptalk.Taptalk.R
+
+class TAPWebBrowserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tap_activity_web_browser)
+    }
+}
