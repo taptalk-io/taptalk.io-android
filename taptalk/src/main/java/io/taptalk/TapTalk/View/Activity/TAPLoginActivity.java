ccd69bb697bc0722c85ce4f0615fde444d691d82
Rionaldo <rionaldo.ling@gmail.com>
2019-04-01 13:18:37 +0700

    change all font to taptalk font

--- taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TAPLoginActivity.java
+++ taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TAPLoginActivity.java
@@ -3,7 +3,6 @@ package io.taptalk.TapTalk.View.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
-import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
