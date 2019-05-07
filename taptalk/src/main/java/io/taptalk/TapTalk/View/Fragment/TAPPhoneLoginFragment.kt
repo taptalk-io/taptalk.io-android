f2796457476ea53da48b359f2306bf2130d6eeb8
Rionaldo <rionaldo.ling@gmail.com>
2019-05-07 14:02:01 +0700

    add checklist for choosen country

--- taptalk/src/main/java/io/taptalk/TapTalk/View/Fragment/TAPPhoneLoginFragment.kt
+++ taptalk/src/main/java/io/taptalk/TapTalk/View/Fragment/TAPPhoneLoginFragment.kt
@@ -15,6 +15,7 @@ import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import io.taptalk.TapTalk.API.View.TapDefaultDataView
 import io.taptalk.TapTalk.Const.TAPDefaultConstant
+import io.taptalk.TapTalk.Const.TAPDefaultConstant.Extras.COUNTRY_ID
 import io.taptalk.TapTalk.Const.TAPDefaultConstant.Extras.COUNTRY_LIST
 import io.taptalk.TapTalk.Const.TAPDefaultConstant.RequestCode.COUNTRY_PICK
 import io.taptalk.TapTalk.Helper.TAPUtils
@@ -126,6 +127,7 @@ class TAPPhoneLoginFragment : Fragment() {
         ll_country_code.setOnClickListener {
             val intent = Intent(context, TAPCountryListActivity::class.java)
             intent.putExtra(COUNTRY_LIST, countryListitems)
+            intent.putExtra(COUNTRY_ID, defaultCountryID)
             startActivityForResult(intent, COUNTRY_PICK)
         }
     }
