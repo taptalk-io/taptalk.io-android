127cc63825cc09a53e91c074cbb61bf5c163c25f
Rionaldo <rionaldo.ling@gmail.com>
2019-06-18 17:42:46 +0700

    create activity for Edit Group

---
+++ taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TAPEditGroupActivity.kt
@@ -0,0 +1,14 @@
+package io.taptalk.TapTalk.View.Activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.taptalk.Taptalk.R
+
+class TAPEditGroupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tap_activity_edit_group)
+    }
+
+}
