ade172d5b8ef4c7a5b3e6a3a8a5fa7c143aabd75
Rionaldo <rionaldo.ling@gmail.com>
2019-06-19 16:57:47 +0700

    off button when name is not change

--- taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TAPEditGroupActivity.kt
+++ taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TAPEditGroupActivity.kt
@@ -1,6 +1,8 @@
 package io.taptalk.TapTalk.View.Activity
 
+import android.app.Activity
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
@@ -9,6 +11,8 @@ import android.view.View
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import io.taptalk.TapTalk.Const.TAPDefaultConstant
+import io.taptalk.TapTalk.Const.TAPDefaultConstant.RequestCode.PICK_GROUP_IMAGE
+import io.taptalk.TapTalk.Helper.TAPUtils
 import io.taptalk.TapTalk.Model.TAPImageURL
 import io.taptalk.TapTalk.ViewModel.TAPGroupViewModel
 import io.taptalk.Taptalk.R
@@ -16,12 +20,17 @@ import kotlinx.android.synthetic.main.tap_activity_edit_group.*
 
 class TAPEditGroupActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
-        when(v?.id) {
+        when (v?.id) {
             R.id.iv_remove_group_picture -> {
                 groupViewModel?.groupData?.roomImage = null
                 civ_group_picture.setImageResource(R.drawable.tap_img_default_avatar)
                 iv_remove_group_picture.visibility = View.GONE
                 btn_edit_group.setBackgroundResource(R.drawable.tap_bg_primary_primarydark_stroke_primarydark_1dp_rounded_6dp_ripple)
+            }
+
+            R.id.ll_change_group_picture -> {
+                TAPUtils.getInstance().animateClickButton(v, 0.95f)
+                TAPUtils.getInstance().pickImageFromGallery(this, PICK_GROUP_IMAGE, false)
             }
         }
     }
@@ -33,6 +42,21 @@ class TAPEditGroupActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.tap_activity_edit_group)
         initViewModel()
         initView()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (resultCode) {
+            Activity.RESULT_OK -> {
+                when(requestCode) {
+                    PICK_GROUP_IMAGE -> {
+                        val groupImage = TAPImageURL(data?.data.toString(), data?.data.toString())
+                        groupViewModel?.groupData?.roomImage = groupImage
+                        loadImage(data?.data.toString())
+                    }
+                }
+            }
+        }
     }
 
     private fun initViewModel() {
@@ -48,12 +72,11 @@ class TAPEditGroupActivity : AppCompatActivity(), View.OnClickListener {
         et_group_name.setText(groupViewModel?.groupData?.roomName ?: "")
 
         iv_remove_group_picture.setOnClickListener(this)
+        ll_change_group_picture.setOnClickListener(this)
 
         if (null != groupViewModel?.groupData?.roomImage) {
             val imageURL = groupViewModel?.groupData?.roomImage
-            Glide.with(this).load(imageURL?.thumbnail)
-                    .apply(RequestOptions().centerCrop()).into(civ_group_picture)
-            iv_remove_group_picture.visibility = View.VISIBLE
+            loadImage(imageURL?.thumbnail ?: "")
         }
 
         btn_edit_group.setBackgroundResource(R.drawable.tap_bg_d9d9d9_rounded_6dp)
@@ -63,12 +86,10 @@ class TAPEditGroupActivity : AppCompatActivity(), View.OnClickListener {
         override fun afterTextChanged(s: Editable?) {}
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            if (s?.isNotEmpty() == true) {
+            if (s?.isNotEmpty() == true && s != groupViewModel?.groupData?.roomName ?: "") {
                 btn_edit_group.setBackgroundResource(R.drawable.tap_bg_primary_primarydark_stroke_primarydark_1dp_rounded_6dp_ripple)
-                groupViewModel?.groupData?.roomName = s.toString()
             } else {
                 btn_edit_group.setBackgroundResource(R.drawable.tap_bg_d9d9d9_rounded_6dp)
-                groupViewModel?.groupData?.roomName = ""
             }
         }
     }
@@ -78,6 +99,13 @@ class TAPEditGroupActivity : AppCompatActivity(), View.OnClickListener {
             true -> et_group_name.background = resources.getDrawable(R.drawable.tap_bg_white_rounded_8dp_stroke_accent_1dp)
             else -> et_group_name.background = resources.getDrawable(R.drawable.tap_bg_white_rounded_8dp_stroke_dcdcdc_1dp)
         }
+    }
+
+    private fun loadImage(imageURL: String) {
+        Glide.with(this).load(imageURL)
+                .apply(RequestOptions().centerCrop()).into(civ_group_picture)
+        iv_remove_group_picture.visibility = View.VISIBLE
+        btn_edit_group.setBackgroundResource(R.drawable.tap_bg_primary_primarydark_stroke_primarydark_1dp_rounded_6dp_ripple)
     }
 
 }
