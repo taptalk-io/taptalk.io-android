d50d6328e1bcf94e499d0a60ddd08da78ac5545a
Rionaldo <rionaldo.ling@gmail.com>
2019-06-20 15:16:43 +0700

    edit group page

--- taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TAPEditGroupActivity.kt
+++ taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TAPEditGroupActivity.kt
@@ -13,8 +13,7 @@ import com.bumptech.glide.request.RequestOptions
 import io.taptalk.TapTalk.Const.TAPDefaultConstant
 import io.taptalk.TapTalk.Const.TAPDefaultConstant.RequestCode.PICK_GROUP_IMAGE
 import io.taptalk.TapTalk.Helper.TAPUtils
-import io.taptalk.TapTalk.Model.TAPImageURL
-import io.taptalk.TapTalk.ViewModel.TAPGroupViewModel
+import io.taptalk.TapTalk.ViewModel.TAPEditGroupViewModel
 import io.taptalk.Taptalk.R
 import kotlinx.android.synthetic.main.tap_activity_edit_group.*
 
@@ -25,17 +24,23 @@ class TAPEditGroupActivity : AppCompatActivity(), View.OnClickListener {
                 groupViewModel?.groupData?.roomImage = null
                 civ_group_picture.setImageResource(R.drawable.tap_img_default_avatar)
                 iv_remove_group_picture.visibility = View.GONE
-                btn_edit_group.setBackgroundResource(R.drawable.tap_bg_primary_primarydark_stroke_primarydark_1dp_rounded_6dp_ripple)
+                groupViewModel?.isGroupPicChanged = groupViewModel?.isGroupPicStartEmpty != true
+                showingButton()
             }
 
             R.id.ll_change_group_picture -> {
                 TAPUtils.getInstance().animateClickButton(v, 0.95f)
                 TAPUtils.getInstance().pickImageFromGallery(this, PICK_GROUP_IMAGE, false)
             }
+
+            R.id.btn_edit_group -> {
+                groupViewModel?.groupData?.roomName = et_group_name.text.toString()
+                //Call Edit API
+            }
         }
     }
 
-    var groupViewModel: TAPGroupViewModel? = null
+    var groupViewModel: TAPEditGroupViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,11 +53,11 @@ class TAPEditGroupActivity : AppCompatActivity(), View.OnClickListener {
         super.onActivityResult(requestCode, resultCode, data)
         when (resultCode) {
             Activity.RESULT_OK -> {
-                when(requestCode) {
+                when (requestCode) {
                     PICK_GROUP_IMAGE -> {
-                        val groupImage = TAPImageURL(data?.data.toString(), data?.data.toString())
-                        groupViewModel?.groupData?.roomImage = groupImage
+                        groupViewModel?.isGroupPicChanged = true
                         loadImage(data?.data.toString())
+                        showingButton()
                     }
                 }
             }
@@ -60,7 +65,7 @@ class TAPEditGroupActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun initViewModel() {
-        groupViewModel = ViewModelProviders.of(this).get(TAPGroupViewModel::class.java)
+        groupViewModel = ViewModelProviders.of(this).get(TAPEditGroupViewModel::class.java)
     }
 
     private fun initView() {
@@ -77,6 +82,8 @@ class TAPEditGroupActivity : AppCompatActivity(), View.OnClickListener {
         if (null != groupViewModel?.groupData?.roomImage) {
             val imageURL = groupViewModel?.groupData?.roomImage
             loadImage(imageURL?.thumbnail ?: "")
+        } else {
+            groupViewModel?.isGroupPicStartEmpty = true
         }
 
         btn_edit_group.setBackgroundResource(R.drawable.tap_bg_d9d9d9_rounded_6dp)
@@ -86,11 +93,13 @@ class TAPEditGroupActivity : AppCompatActivity(), View.OnClickListener {
         override fun afterTextChanged(s: Editable?) {}
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            if (s?.isNotEmpty() == true && s != groupViewModel?.groupData?.roomName ?: "") {
-                btn_edit_group.setBackgroundResource(R.drawable.tap_bg_primary_primarydark_stroke_primarydark_1dp_rounded_6dp_ripple)
-            } else {
-                btn_edit_group.setBackgroundResource(R.drawable.tap_bg_d9d9d9_rounded_6dp)
+            if (s?.isNotEmpty() == true && s.toString() != groupViewModel?.groupData?.roomName ?: "") {
+                groupViewModel?.isGroupNameChanged = true
+            } else if (s?.isNotEmpty() == true && s.toString() == groupViewModel?.groupData?.roomName ?: "") {
+                groupViewModel?.isGroupNameChanged = false
             }
+
+            showingButton()
         }
     }
 
@@ -105,7 +114,14 @@ class TAPEditGroupActivity : AppCompatActivity(), View.OnClickListener {
         Glide.with(this).load(imageURL)
                 .apply(RequestOptions().centerCrop()).into(civ_group_picture)
         iv_remove_group_picture.visibility = View.VISIBLE
-        btn_edit_group.setBackgroundResource(R.drawable.tap_bg_primary_primarydark_stroke_primarydark_1dp_rounded_6dp_ripple)
+    }
+
+    private fun showingButton() {
+        if (groupViewModel?.isGroupPicChanged == false && groupViewModel?.isGroupNameChanged == false) {
+            btn_edit_group.setBackgroundResource(R.drawable.tap_bg_d9d9d9_rounded_6dp)
+        } else {
+            btn_edit_group.setBackgroundResource(R.drawable.tap_bg_primary_primarydark_stroke_primarydark_1dp_rounded_6dp_ripple)
+        }
     }
 
 }
---
+++ taptalk/src/main/java/io/taptalk/TapTalk/ViewModel/TAPEditGroupViewModel.kt
@@ -0,0 +1,31 @@
+package io.taptalk.TapTalk.ViewModel
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import io.taptalk.TapTalk.Model.TAPRoomModel
+
+class TAPEditGroupViewModel(application: Application) : AndroidViewModel(application) {
+    var groupData: TAPRoomModel? = null
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var isGroupPicChanged: Boolean = false
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var isGroupPicStartEmpty: Boolean = false
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var isGroupNameChanged: Boolean = false
+        get() = field
+        set(value) {
+            field = value
+        }
+}
