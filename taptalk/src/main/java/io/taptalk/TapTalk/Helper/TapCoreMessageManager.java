571d7d521e88466cdd0fd506e0dc1fb6ae99ab0a
Jefry Lorentono <jefry@moselo.com>
2019-07-26 15:47:11 +0700

    tapcore

---
+++ taptalk/src/main/java/io/taptalk/TapTalk/Helper/TapCoreMessageManager.java
@@ -0,0 +1,5 @@
+package io.taptalk.TapTalk.Helper;
+
+public class TapCoreMessageManager {
+
+}
---
+++ taptalk/src/main/java/io/taptalk/TapTalk/Helper/TapCoreRoomListManager.java
@@ -0,0 +1,11 @@
+package io.taptalk.TapTalk.Helper;
+
+public class TapCoreRoomListManager {
+    // TODO: 2019-07-25 addChatListener();
+    // TODO: 2019-07-25 removeChatListener();
+
+
+    // TODO: 2019-07-25 getRoomList(); 
+    // TODO: 2019-07-25 getNewAndUpdated();
+    // TODO: 2019-07-25 getMessageRoomListAndUnread();
+}
