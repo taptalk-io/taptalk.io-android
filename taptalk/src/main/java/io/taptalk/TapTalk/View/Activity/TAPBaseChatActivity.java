2ac45c4a151d58f7ceb1772c94cca0ab2c018a25
Kevin <kevin@taptalk.io>
2019-10-16 16:56:22 +0700

    - fixed auto clean query & timestamp

--- taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TAPBaseChatActivity.java
+++ taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TAPBaseChatActivity.java
@@ -1,17 +1,10 @@
 package io.taptalk.TapTalk.View.Activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.content.LocalBroadcastManager;
 
 import io.taptalk.TapTalk.Helper.SwipeBackLayout.SwipeBackActivity;
 import io.taptalk.TapTalk.Helper.TAPUtils;
-import io.taptalk.TapTalk.Helper.TapTalk;
-import io.taptalk.TapTalk.Manager.TAPChatManager;
-
-import static io.taptalk.TapTalk.Const.TAPDefaultConstant.Extras.ROOM_ID;
-import static io.taptalk.TapTalk.Const.TAPDefaultConstant.RELOAD_ROOM_LIST;
 
 public abstract class TAPBaseChatActivity extends SwipeBackActivity {
 
@@ -32,13 +25,5 @@ public abstract class TAPBaseChatActivity extends SwipeBackActivity {
     protected void onPause() {
         super.onPause();
         TAPUtils.getInstance().dismissKeyboard(this);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Intent intent = new Intent(RELOAD_ROOM_LIST);
-        intent.putExtra(ROOM_ID, TAPChatManager.getInstance().getOpenRoom());
-        LocalBroadcastManager.getInstance(TapTalk.appContext).sendBroadcast(intent);
     }
 }
