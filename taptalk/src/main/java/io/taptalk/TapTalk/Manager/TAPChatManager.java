ba2363764c9d52f5f6086f3c29e90c64c00311f8
Kevin <kevin@taptalk.io>
2020-06-16 19:28:17 +0700

    - Fixed missing instance key on refresh token expired handling

--- taptalk/src/main/java/io/taptalk/TapTalk/Manager/TAPChatManager.java
+++ taptalk/src/main/java/io/taptalk/TapTalk/Manager/TAPChatManager.java
@@ -1618,10 +1618,12 @@ public class TAPChatManager {
 
     private void insertToList(Map<String, TAPMessageModel> hashMap) {
         try {
+            List<TAPMessageEntity> messagesToInsert = new ArrayList<>();
             for (Map.Entry<String, TAPMessageModel> message : hashMap.entrySet()) {
-                saveMessages.add(convertToEntity(message.getValue()));
+                messagesToInsert.add(convertToEntity(message.getValue()));
             }
-        } catch (ConcurrentModificationException e) { // FIXME: 20 Dec 2019
+            saveMessages.addAll(messagesToInsert);
+        } catch (ConcurrentModificationException e) {
             e.printStackTrace();
         }
     }
