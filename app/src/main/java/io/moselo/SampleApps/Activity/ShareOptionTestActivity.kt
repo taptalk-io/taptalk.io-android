a96c15f2d6f88da678dca6538006a5f47b3797ba
miftah <miftah@taptalk.io>
2021-04-05 18:16:59 +0700

    add method delete file cache, and show hide loading popup

--- app/src/main/java/io/moselo/SampleApps/Activity/ShareOptionTestActivity.kt
+++ app/src/main/java/io/moselo/SampleApps/Activity/ShareOptionTestActivity.kt
@@ -4,20 +4,24 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
-import android.provider.OpenableColumns
 import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
+import androidx.core.content.ContextCompat
 import io.taptalk.TapTalk.Helper.TAPFileUtils
+import io.taptalk.TapTalk.Helper.TAPUtils
 import io.taptalk.TapTalk.Listener.TapCoreSendMessageListener
-import io.taptalk.TapTalk.Manager.TAPChatManager
 import io.taptalk.TapTalk.Manager.TapCoreMessageManager
 import io.taptalk.TapTalk.Model.TAPImageURL
 import io.taptalk.TapTalk.Model.TAPMessageModel
 import io.taptalk.TapTalk.Model.TAPRoomModel
 import io.taptalk.TapTalkSample.R
 import kotlinx.android.synthetic.main.activity_share_option_test.*
+import kotlinx.android.synthetic.main.activity_share_option_test.popup_loading
+import kotlinx.android.synthetic.main.tap_fragment_login_verification.*
 import java.io.File
 
 class ShareOptionTestActivity : AppCompatActivity() {
@@ -69,17 +73,19 @@ class ShareOptionTestActivity : AppCompatActivity() {
     private fun handleFileType(intent: Intent) {
         (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let {
             val file = File(TAPFileUtils.getInstance().getFilePath(this, it))
-            TapCoreMessageManager.getInstance().sendFileMessage(file, roomModel, object :TapCoreSendMessageListener() {
+            TapCoreMessageManager.getInstance().sendFileMessage(file, roomModel, object : TapCoreSendMessageListener() {
                 override fun onStart(message: TAPMessageModel?) {
                     super.onStart(message)
                     runOnUiThread {
-                        pb_loading.visibility = View.VISIBLE
+                        showPopupLoading(getString(R.string.tap_loading))
                     }
                 }
+
                 override fun onSuccess(message: TAPMessageModel?) {
                     super.onSuccess(message)
                     runOnUiThread {
-                        pb_loading.visibility = View.GONE
+                        hidePopupLoading()
+                        deleteCache()
                     }
                 }
             })
@@ -88,17 +94,18 @@ class ShareOptionTestActivity : AppCompatActivity() {
 
     private fun handleVideoType(intent: Intent) {
         (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let {
-            TapCoreMessageManager.getInstance().sendVideoMessage(it, "share video", roomModel, object :TapCoreSendMessageListener() {
+            TapCoreMessageManager.getInstance().sendVideoMessage(it, "share video", roomModel, object : TapCoreSendMessageListener() {
                 override fun onStart(message: TAPMessageModel?) {
                     super.onStart(message)
                     runOnUiThread {
-                        pb_loading.visibility = View.VISIBLE
+                        showPopupLoading(getString(R.string.tap_loading))
                     }
                 }
+
                 override fun onSuccess(message: TAPMessageModel?) {
                     super.onSuccess(message)
                     runOnUiThread {
-                        pb_loading.visibility = View.GONE
+                        hidePopupLoading()
                     }
                 }
             })
@@ -111,13 +118,14 @@ class ShareOptionTestActivity : AppCompatActivity() {
                 override fun onStart(message: TAPMessageModel?) {
                     super.onStart(message)
                     runOnUiThread {
-                        pb_loading.visibility = View.VISIBLE
+                        showPopupLoading(getString(R.string.tap_loading))
                     }
                 }
+
                 override fun onSuccess(message: TAPMessageModel?) {
                     super.onSuccess(message)
                     runOnUiThread {
-                        pb_loading.visibility = View.GONE
+                        hidePopupLoading()
                     }
                 }
             })
@@ -125,13 +133,52 @@ class ShareOptionTestActivity : AppCompatActivity() {
     }
 
     private fun handleTextType(intent: Intent) {
-        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
-            TapCoreMessageManager.getInstance().sendTextMessage(it, roomModel, object : TapCoreSendMessageListener() {
-                override fun onSuccess(message: TAPMessageModel?) {
-                    super.onSuccess(message)
-                    Toast.makeText(this@ShareOptionTestActivity, "SUCCES SEND MESSAGE",Toast.LENGTH_SHORT).show()
+        if (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) == null) {
+            //text type with string data
+            intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+                TapCoreMessageManager.getInstance().sendTextMessage(it, roomModel, object : TapCoreSendMessageListener() {
+                    override fun onSuccess(message: TAPMessageModel?) {
+                        super.onSuccess(message)
+                        Toast.makeText(this@ShareOptionTestActivity, "SUCCES SEND MESSAGE", Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }
+        } else {
+            // text type with file data
+            handleFileType(intent)
+        }
+
+    }
+
+    private fun showPopupLoading(message: String) {
+        popup_loading.findViewById<ImageView>(R.id.iv_loading_image).setImageDrawable(ContextCompat.getDrawable(this, R.drawable.tap_ic_loading_progress_circle_white))
+        if (null == popup_loading.findViewById<ImageView>(R.id.iv_loading_image).animation)
+            TAPUtils.rotateAnimateInfinitely(this, popup_loading.findViewById<ImageView>(R.id.iv_loading_image))
+        popup_loading.findViewById<TextView>(R.id.tv_loading_text)?.text = message
+        popup_loading.findViewById<FrameLayout>(R.id.popup_loading).visibility = View.VISIBLE
+    }
+
+    private fun hidePopupLoading() {
+        popup_loading.findViewById<FrameLayout>(R.id.popup_loading).visibility = View.GONE
+    }
+
+    fun deleteCache() {
+        try {
+            val dir: File = cacheDir
+            deleteDir(dir)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun deleteDir(dir: File?) {
+        if (dir != null && dir.isDirectory) {
+            val children = dir.listFiles()
+            for (i in children.indices) {
+                if (children[i].name.equals("share")) {
+                    children[i].deleteRecursively()
                 }
-            })
+            }
         }
     }
 }
