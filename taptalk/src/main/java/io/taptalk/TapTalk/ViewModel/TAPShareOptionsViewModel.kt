822a91044b09850432facb865d7755bd833c4cda
Mu'adz Abdurrahman <muadz@taptalk.io>
2021-04-29 15:46:54 +0700

    share options new assets

--- taptalk/src/main/java/io/taptalk/TapTalk/ViewModel/TAPShareOptionsViewModel.kt
+++ taptalk/src/main/java/io/taptalk/TapTalk/ViewModel/TAPShareOptionsViewModel.kt
@@ -2,21 +2,16 @@ package io.taptalk.TapTalk.ViewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import io.taptalk.TapTalk.Data.RecentSearch.TAPRecentSearchEntity
-import io.taptalk.TapTalk.Manager.TAPDataManager
-import io.taptalk.TapTalk.Model.TAPMessageModel
 import io.taptalk.TapTalk.Model.TAPRoomListModel
 import io.taptalk.TapTalk.Model.TAPRoomModel
-import io.taptalk.TapTalk.Model.TAPSearchChatModel
 
-class TAPShareOptionsViewModel(application: Application, instanceKey: String?): AndroidViewModel(application) {
+class TAPShareOptionsViewModel(application: Application): AndroidViewModel(application) {
 
-    class TAPShareOptionsViewModelFactory(private val application: Application, private val instanceKey: String?) : ViewModelProvider.Factory {
+    class TAPShareOptionsViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            return TAPShareOptionsViewModel(application, instanceKey) as T
+            return TAPShareOptionsViewModel(application) as T
         }
     }
 
@@ -25,15 +20,15 @@ class TAPShareOptionsViewModel(application: Application, instanceKey: String?): 
     var selectedRooms: MutableMap<String, TAPRoomListModel>? = LinkedHashMap()
     var isSelecting = false
 
-//    var recentSearchList: LiveData<List<TAPRecentSearchEntity>>? = TAPDataManager.getInstance(instanceKey).recentSearchLive
-    var searchRoomResults: List<TAPRoomListModel>? = ArrayList()
+    var searchRoomResults: ArrayList<TAPRoomListModel>? = ArrayList()
     var recentSearches: List<TAPRoomListModel>? = ArrayList()
+    var personalContacts: ArrayList<TAPRoomListModel>? = ArrayList()
+    var groupContacts: ArrayList<TAPRoomListModel>? = ArrayList()
     var searchRoomPointer: MutableMap<String, TAPRoomModel>? = LinkedHashMap()
     var searchKeyword: String? = null
     var pendingSearch: String? = null
     var searchState: Int? = 0
 
-    val STATE_RECENT_SEARCHES = 0
     val STATE_IDLE = 1
     val STATE_SEARCHING = 2
     val STATE_PENDING = 3
@@ -46,7 +41,7 @@ class TAPShareOptionsViewModel(application: Application, instanceKey: String?): 
         return selectedRooms?.size
     }
 
-    fun setSearchResults(searchResults: List<TAPRoomListModel>) {
+    fun setSearchResults(searchResults: ArrayList<TAPRoomListModel>) {
         this.searchRoomResults = searchResults
         searchRoomPointer?.clear()
         for (result in searchResults) {
@@ -65,8 +60,16 @@ class TAPShareOptionsViewModel(application: Application, instanceKey: String?): 
         }
     }
 
+    fun getSearchResults(): List<TAPRoomListModel>? {
+        searchRoomResults?.addAll(groupContacts!!)
+        searchRoomResults?.addAll(personalContacts!!)
+        return searchRoomResults
+    }
+
     fun clearSearchResults() {
-        searchRoomResults?.toMutableList()?.clear()
+        personalContacts?.clear()
+        groupContacts?.clear()
+        searchRoomResults?.clear()
         roomPointer?.toMutableMap()?.clear()
     }
 
