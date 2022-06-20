a43ba913af266b44fbddb590617e4ff967111c5a
Mu'adz Abdurrahman <muadz@taptalk.io>
2022-06-20 11:42:11 +0700

    delete account page UI

---
+++ taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TapDeleteAccountActivity.kt
@@ -0,0 +1,12 @@
+package io.taptalk.TapTalk.View.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.taptalk.TapTalk.R
+
+class TapDeleteAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tap_activity_delete_account)
+    }
+}
