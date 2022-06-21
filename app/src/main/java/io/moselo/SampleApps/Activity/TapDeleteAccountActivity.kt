020d24af64d555275b4e5611f0276ac7b4fa5272
Mu'adz Abdurrahman <muadz@taptalk.io>
2022-06-21 14:57:09 +0700

    move delete account page to sample apps

--- app/src/main/java/io/moselo/SampleApps/Activity/TapDeleteAccountActivity.kt
+++ app/src/main/java/io/moselo/SampleApps/Activity/TapDeleteAccountActivity.kt
@@ -1,10 +1,10 @@
-package io.taptalk.TapTalk.View.Activity
+package io.moselo.SampleApps.Activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import io.taptalk.TapTalk.R
+import io.taptalk.TapTalk.View.Activity.TAPBaseActivity
+import io.taptalk.TapTalkSample.R
 
-class TapDeleteAccountActivity : AppCompatActivity() {
+class TapDeleteAccountActivity : TAPBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tap_activity_delete_account)
