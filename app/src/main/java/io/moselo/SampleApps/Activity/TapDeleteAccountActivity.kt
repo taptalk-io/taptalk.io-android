f367186f1b3aa7bf271e3d2b6f27f478f76dfed5
Mu'adz Abdurrahman <muadz@taptalk.io>
2022-06-22 14:12:28 +0700

    add delete account pop ups

--- app/src/main/java/io/moselo/SampleApps/Activity/TapDeleteAccountActivity.kt
+++ app/src/main/java/io/moselo/SampleApps/Activity/TapDeleteAccountActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import io.taptalk.TapTalk.Const.TAPDefaultConstant.Extras
+import io.taptalk.TapTalk.Helper.TapTalkDialog
 import io.taptalk.TapTalk.View.Activity.TAPBaseActivity
 import io.taptalk.TapTalkSample.R
 import kotlinx.android.synthetic.main.tap_activity_delete_account.*
@@ -12,8 +13,32 @@ class TapDeleteAccountActivity : TAPBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tap_activity_delete_account)
+
         cb_agree.setOnCheckedChangeListener { _, isChecked ->
             btn_delete.isEnabled = isChecked
+        }
+        btn_delete.setOnClickListener {
+            // TODO: 22/06/22 call check state api MU
+            // TODO: 22/06/22 for testing purposes only MU
+            if (et_note.text.toString().isEmpty()) {
+                TapTalkDialog.Builder(this)
+                    .setTitle(getString(R.string.tap_wording_oops_only_admin))
+                    .setMessage(getString(R.string.tap_wording_sign_to_another_member))
+                    .setDialogType(TapTalkDialog.DialogType.DEFAULT)
+                    .setPrimaryButtonTitle(getString(R.string.tap_ok))
+                    .setPrimaryButtonListener {  }
+                    .show()
+            } else {
+                TapTalkDialog.Builder(this)
+                    .setTitle(getString(R.string.tap_delete_my_account))
+                    .setMessage(getString(R.string.tap_wording_delete_account))
+                    .setDialogType(TapTalkDialog.DialogType.ERROR_DIALOG)
+                    .setPrimaryButtonTitle(getString(R.string.tap_delete))
+                    .setPrimaryButtonListener { /* TODO: move to otp verification MU */ }
+                    .setSecondaryButtonTitle(getString(R.string.tap_cancel))
+                    .setSecondaryButtonListener {  }
+                    .show()
+            }
         }
     }
 
