3a6c536e08b7c1ce84d9fff11d839e90ea37473b
Mu'adz Abdurrahman <muadz@taptalk.io>
2022-09-12 15:16:57 +0700

    update app version

--- taptalk/src/main/java/io/taptalk/TapTalk/Helper/TapTalk.java
+++ taptalk/src/main/java/io/taptalk/TapTalk/Helper/TapTalk.java
@@ -282,7 +282,7 @@ public class TapTalk implements LifecycleObserver {
         }
 
         if (sdkVersion.isEmpty()) {
-            String version = "2.5.0";
+            String version = "2.6.0";
             sdkVersion = String.format("%s-%s", version, BuildConfig.BUILD_TYPE);
         }
     }
