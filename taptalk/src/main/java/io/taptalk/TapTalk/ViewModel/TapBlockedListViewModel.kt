4e0a2f3d77a94cba43f97ed9b1ed707a9b9a2107
Mu'adz Abdurrahman <muadz@taptalk.io>
2022-10-27 19:51:29 +0700

    add adapter class for blocked list page

--- taptalk/src/main/java/io/taptalk/TapTalk/ViewModel/TapBlockedListViewModel.kt
+++ taptalk/src/main/java/io/taptalk/TapTalk/ViewModel/TapBlockedListViewModel.kt
@@ -4,11 +4,5 @@ import androidx.lifecycle.ViewModel
 import io.taptalk.TapTalk.Model.TAPUserModel
 
 class TapBlockedListViewModel : ViewModel() {
-
-    enum class State {
-        VIEW, EDIT
-    }
-
-    var editState = State.VIEW
     var blockedList = ArrayList<TAPUserModel>()
 }
