4d066f25ffd5d46a6e8c9937f0122871e1d045c3
Mu'adz Abdurrahman <muadz@taptalk.io>
2022-10-27 14:33:50 +0700

    convert class to kotlin

---
+++ taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TAPBlockedListActivity.kt
@@ -0,0 +1,91 @@
+package io.taptalk.TapTalk.View.Activity
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.taptalk.TapTalk.Const.TAPDefaultConstant.Extras
+import io.taptalk.TapTalk.Helper.OverScrolled.OverScrollDecoratorHelper
+import io.taptalk.TapTalk.Helper.TAPUtils
+import io.taptalk.TapTalk.Model.ResponseModel.TapContactListModel
+import io.taptalk.TapTalk.Model.TAPUserModel
+import io.taptalk.TapTalk.R
+import io.taptalk.TapTalk.View.Adapter.TapContactListAdapter
+import io.taptalk.TapTalk.ViewModel.TAPContactListViewModel
+import io.taptalk.TapTalk.ViewModel.TAPContactListViewModel.TAPContactListViewModelFactory
+import kotlinx.android.synthetic.main.tap_activity_blocked_list.*
+
+class TAPBlockedListActivity : TAPBaseActivity() {
+    private var adapter: TapContactListAdapter? = null
+    private var vm: TAPContactListViewModel? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tap_activity_blocked_list)
+        initViewModel()
+        initView()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.tap_stay, R.anim.tap_slide_right)
+    }
+
+    private fun initViewModel() {
+        vm = ViewModelProvider(
+            this,
+            TAPContactListViewModelFactory(
+                application, instanceKey
+            )
+        )
+            .get(TAPContactListViewModel::class.java)
+
+        //Dummy Contacts
+        if (vm!!.contactList.size == 0) {
+            val u1 = TAPUserModel("u1", "Dummy Spam 1")
+            val u2 = TAPUserModel("u2", "Dummy Spam 2")
+            val u3 = TAPUserModel("u3", "Tummy Spam 3")
+            vm!!.filteredContacts.add(u1)
+            vm!!.filteredContacts.add(u2)
+            vm!!.filteredContacts.add(u3)
+        }
+        //End Dummy
+    }
+
+    private fun initView() {
+        window.setBackgroundDrawable(null)
+        adapter = TapContactListAdapter(
+            instanceKey,
+            TAPUtils.generateContactListForRecycler(
+                vm!!.filteredContacts,
+                TapContactListModel.TYPE_DEFAULT_CONTACT_LIST
+            )
+        )
+        rv_blocked_list.adapter = adapter
+        rv_blocked_list.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+        OverScrollDecoratorHelper.setUpOverScroll(
+            rv_blocked_list,
+            OverScrollDecoratorHelper.ORIENTATION_VERTICAL
+        )
+        iv_button_back.setOnClickListener { onBackPressed() }
+    }
+
+    companion object {
+        fun start(
+            context: Context,
+            instanceKey: String?
+        ) {
+            val intent = Intent(context, TAPBlockedListActivity::class.java)
+            intent.putExtra(Extras.INSTANCE_KEY, instanceKey)
+            context.startActivity(intent)
+            if (context is Activity) {
+                context.overridePendingTransition(R.anim.tap_slide_up, R.anim.tap_stay)
+            }
+        }
+    }
+}
