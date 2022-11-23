a8d0227288335d330e4425b1e8c9eb4f883eca31
Mu'adz Abdurrahman <muadz@taptalk.io>
2022-11-23 23:38:35 +0700

    implement get message detail API

--- taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TapMessageInfoActivity.kt
+++ taptalk/src/main/java/io/taptalk/TapTalk/View/Activity/TapMessageInfoActivity.kt
@@ -4,11 +4,17 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import io.taptalk.TapTalk.API.View.TAPDefaultDataView
 import io.taptalk.TapTalk.Const.TAPDefaultConstant
+import io.taptalk.TapTalk.Manager.TAPDataManager
 import io.taptalk.TapTalk.Manager.TapUI
+import io.taptalk.TapTalk.Model.ResponseModel.TapGetMessageDetailResponse
 import io.taptalk.TapTalk.Model.ResponseModel.TapMessageRecipientModel
+import io.taptalk.TapTalk.Model.TAPErrorModel
 import io.taptalk.TapTalk.Model.TAPMessageModel
 import io.taptalk.TapTalk.R
 import io.taptalk.TapTalk.View.Adapter.TapMessageInfoAdapter
@@ -42,35 +48,69 @@ class TapMessageInfoActivity : TAPBaseActivity() {
         if (intent.getParcelableExtra<TAPMessageModel>(TAPDefaultConstant.Extras.MESSAGE) != null) {
             vm.message = intent.getParcelableExtra(TAPDefaultConstant.Extras.MESSAGE)
         }
-        // TODO: get list from API MU
-        val messageInfoList = ArrayList<TapMessageRecipientModel>()
         val resultList = ArrayList<TapMessageRecipientModel>()
         val adapter = TapMessageInfoAdapter(resultList)
         rv_message_info.adapter = adapter
         rv_message_info.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         rv_message_info.setHasFixedSize(true)
 
-        for (item in messageInfoList) {
-            if (item.readTime != null && item.readTime > 0) {
-                vm.readList.add(item)
-            } else if (item.deliveredTime != null && item.deliveredTime > 0) {
-                vm.deliveredList.add(item)
-            }
-        }
-        if (vm.readList.isNotEmpty() && !TapUI.getInstance(instanceKey).isReadStatusHidden) {
-            resultList.add(TapMessageRecipientModel(vm.readList.size.toLong(), null))
-            resultList.addAll(vm.readList)
-            if (vm.deliveredList.isNotEmpty()) {
-                resultList.add(TapMessageRecipientModel(null, vm.deliveredList.size.toLong()))
-                resultList.addAll(vm.deliveredList)
-            }
-        } else {
-            resultList.add(TapMessageRecipientModel(null, vm.deliveredList.size.toLong()))
-            if (vm.deliveredList.isNotEmpty()) {
-                resultList.addAll(vm.deliveredList)
-            }
-        }
-        adapter.items = resultList
+    }
 
+    override fun onResume() {
+        super.onResume()
+        TAPDataManager.getInstance(instanceKey).getMessageDetails(vm.message?.messageID, messageInfoView)
+    }
+
+    private val messageInfoView = object : TAPDefaultDataView<TapGetMessageDetailResponse>() {
+        override fun startLoading() {
+            super.startLoading()
+            ll_loading.visibility = View.VISIBLE
+        }
+
+        override fun endLoading() {
+            super.endLoading()
+            ll_loading.visibility = View.GONE
+        }
+
+        override fun onSuccess(response: TapGetMessageDetailResponse?) {
+            super.onSuccess(response)
+            vm.readList.clear()
+            val resultList = ArrayList<TapMessageRecipientModel>()
+            if (response?.readBy != null) {
+                vm.readList.addAll(ArrayList(response.readBy))
+            }
+            vm.deliveredList.clear()
+            if (response?.deliveredTo != null) {
+                vm.deliveredList.addAll(ArrayList(response.deliveredTo))
+            }
+            if (vm.readList.isNotEmpty() && !TapUI.getInstance(instanceKey).isReadStatusHidden) {
+                resultList.add(TapMessageRecipientModel(vm.readList.size.toLong(), null))
+                resultList.addAll(vm.readList)
+                if (vm.deliveredList.isNotEmpty()) {
+                    resultList.add(TapMessageRecipientModel(null, vm.deliveredList.size.toLong()))
+                    resultList.addAll(vm.deliveredList)
+                }
+            } else {
+                resultList.add(TapMessageRecipientModel(null, vm.deliveredList.size.toLong()))
+                if (vm.deliveredList.isNotEmpty()) {
+                    resultList.addAll(vm.deliveredList)
+                }
+            }
+            (rv_message_info.adapter as TapMessageInfoAdapter).items = resultList
+        }
+
+        override fun onError(error: TAPErrorModel?) {
+            super.onError(error)
+            endLoading()
+            Toast.makeText(this@TapMessageInfoActivity, error?.message, Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        override fun onError(errorMessage: String?) {
+            super.onError(errorMessage)
+            endLoading()
+            Toast.makeText(this@TapMessageInfoActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
