e86203d154ef7ba8fafe718352b07b66cef4c1a9
Mu'adz Abdurrahman <muadz@taptalk.io>
2022-11-23 14:02:36 +0700

    add models for message details API

---
+++ taptalk/src/main/java/io/taptalk/TapTalk/Model/ResponseModel/TapMessageRecipientModel.kt
@@ -0,0 +1,25 @@
+package io.taptalk.TapTalk.Model.ResponseModel
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.taptalk.TapTalk.Model.TAPImageURL
+
+data class TapMessageRecipientModel(
+
+    @field:JsonProperty("userImageURL")
+    val userImageURL: TAPImageURL? = null,
+
+    @field:JsonProperty("xcUserID")
+    val xcUserID: String? = null,
+
+    @field:JsonProperty("readTime")
+    val readTime: Long? = null,
+
+    @field:JsonProperty("fullname")
+    val fullname: String? = null,
+
+    @field:JsonProperty("deliveredTime")
+    val deliveredTime: Long? = null,
+
+    @field:JsonProperty("userID")
+    val userID: String? = null
+)
