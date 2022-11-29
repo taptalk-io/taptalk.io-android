5d2d81c984820bd569d32f0c661b16850ffe4698
Kevin Reynaldo <kevin@taptalk.io>
2022-11-29 12:48:01 +0700

    • Updated version to 1.4.0.2 • Updated publications

--- taptalk/src/main/java/io/taptalk/TapTalk/Helper/TapTalk.java
+++ taptalk/src/main/java/io/taptalk/TapTalk/Helper/TapTalk.java
@@ -283,7 +283,7 @@ public class TapTalk implements LifecycleObserver {
         }
 
         if (sdkVersion.isEmpty()) {
-            String version = "1.4.0.1";
+            String version = "1.4.0.2";
             sdkVersion = String.format("%s-%s", version, BuildConfig.BUILD_TYPE);
         }
     }
