775401923e26b408119b1c73fc2ac3a7d2ae067c
Kevin Reynaldo <kevin@tapta.lk>
2023-06-20 16:04:13 +0700

    • Handled open login page if user is not logged in • Added url validation in DeepLinkActivity

--- app/src/main/java/io/moselo/SampleApps/Activity/TapDeepLinkActivity.java
+++ app/src/main/java/io/moselo/SampleApps/Activity/TapDeepLinkActivity.java
@@ -1,12 +1,14 @@
 package io.moselo.SampleApps.Activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 
+import io.taptalk.TapTalk.Helper.TapTalk;
 import io.taptalk.TapTalk.Listener.TapCoreGetRoomListener;
 import io.taptalk.TapTalk.Manager.TapCoreChatRoomManager;
 import io.taptalk.TapTalk.Manager.TapUI;
@@ -21,40 +23,59 @@ public class TapDeepLinkActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         if (intent != null && intent.getData() != null) {
-            List<String> pathSegments = intent.getData().getPathSegments();
-            //Log.e(">>>>", "DeepLinkActivity data: " + data);
-            //Log.e(">>>>", "DeepLinkActivity pathSegments: " + TAPUtils.toJsonString(pathSegments));
+            Uri data = intent.getData();
+//            Log.e(">>>>", "DeepLinkActivity data: " + data);
+            if (data.getHost().equals("web.taptalk.io")) {
+                if (TapTalk.isAuthenticated()) {
+                    List<String> pathSegments = data.getPathSegments();
+//                    Log.e(">>>>", "DeepLinkActivity pathSegments: " + TAPUtils.toJsonString(pathSegments));
 
-            if (pathSegments != null && !pathSegments.isEmpty()) {
-                // Get room ID from last path segment
-                String roomID = pathSegments.get(pathSegments.size() - 1);
+                    if (pathSegments != null && !pathSegments.isEmpty()) {
+                        // Get room ID from last path segment
+                        String roomID = pathSegments.get(pathSegments.size() - 1);
 
-                // Get room details from obtained room ID
-                TapCoreChatRoomManager.getInstance().getGroupChatRoom(roomID, new TapCoreGetRoomListener() {
-                    @Override
-                    public void onSuccess(TAPRoomModel room) {
-                        //Log.e(">>>>", "DeepLinkActivity get room success: " + room.getName());
-                        // Open chat room
-                        TapUI.getInstance().openChatRoomWithRoomModel(TapDeepLinkActivity.this, room);
+                        // Get room details from obtained room ID
+                        TapCoreChatRoomManager.getInstance().getGroupChatRoom(roomID, new TapCoreGetRoomListener() {
+                            @Override
+                            public void onSuccess(TAPRoomModel room) {
+//                                Log.e(">>>>", "DeepLinkActivity get room success: " + room.getName());
+                                // Open chat room
+                                TapUI.getInstance().openChatRoomWithRoomModel(TapDeepLinkActivity.this, room);
+                            }
+
+                            @Override
+                            public void onError(String errorCode, String errorMessage) {
+//                                Log.e(">>>>", "DeepLinkActivity get room error: " + errorMessage);
+                                // Fallback if room data is not found
+                                TapUI.getInstance().openRoomList(TapDeepLinkActivity.this);
+                            }
+                        });
                     }
-
-                    @Override
-                    public void onError(String errorCode, String errorMessage) {
-                        //Log.e(">>>>", "DeepLinkActivity get room error: " + errorMessage);
-                        // Fallback if room data is not found
+                    else {
+//                        Log.e(">>>>", "DeepLinkActivity room ID not found");
+                        // Fallback if room ID is not found
                         TapUI.getInstance().openRoomList(TapDeepLinkActivity.this);
                     }
-                });
+                }
+                else {
+//                    Log.e(">>>>", "DeepLinkActivity user is not logged in");
+                    // User is not logged in
+                    Intent mainActivityIntent = new Intent(this, MainActivity.class);
+                    startActivity(mainActivityIntent);
+                }
             }
             else {
-                // Fallback if room ID is not found
-                TapUI.getInstance().openRoomList(TapDeepLinkActivity.this);
+//                Log.e(">>>>", "DeepLinkActivity url is not chat");
+                // Fallback if tapped url is not chat
+                Intent mainActivityIntent = new Intent(this, MainActivity.class);
+                startActivity(mainActivityIntent);
             }
         }
         else {
-            //Log.e(">>>>", "DeepLinkActivity data null");
+//            Log.e(">>>>", "DeepLinkActivity data null");
             // Fallback if data is null
-            TapUI.getInstance().openRoomList(TapDeepLinkActivity.this);
+            Intent mainActivityIntent = new Intent(this, MainActivity.class);
+            startActivity(mainActivityIntent);
         }
 
         finish();
