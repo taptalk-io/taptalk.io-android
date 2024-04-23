947e8e301ef6565d19c30305d279aba0bb5757dc
Kevin Reynaldo <kevin@tapta.lk>
2024-04-23 09:39:58 +0700

    - Updated version to 2.15.5

--- taptalk/src/main/java/io/taptalk/TapTalk/Helper/TapTalk.java
+++ taptalk/src/main/java/io/taptalk/TapTalk/Helper/TapTalk.java
@@ -287,7 +287,7 @@ public class TapTalk implements LifecycleObserver {
 
         if (sdkVersion.isEmpty()) {
             // Use hard-coded string
-            String version = "2.15.4";
+            String version = "2.15.5";
             sdkVersion = String.format("%s-%s", version, BuildConfig.BUILD_TYPE);
         }
     }
